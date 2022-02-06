16c9b186e0ddb408fe0c82e0618b706faa4ba1bb
WhatuWannaDo <ebategovrotpizdec@gmail.com>
2022-02-06 18:38:32 +0300

    time bug fix

--- app/src/main/java/com/example/myreminders/MVVM/View/Completed/showFullCompletedReminder.kt
+++ app/src/main/java/com/example/myreminders/MVVM/View/Completed/showFullCompletedReminder.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.example.myreminders.R
 import kotlinx.android.synthetic.main.fragment_show_full_completed_reminder.view.*
+import java.text.SimpleDateFormat
 
 
 class showFullCompletedReminder : Fragment() {
@@ -20,9 +21,10 @@ class showFullCompletedReminder : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_show_full_completed_reminder, container, false)
 
+        val sdf = SimpleDateFormat("dd.M.yyyy")
         view.fullCompleteHeader.setText(arguments.argsForFullCompleted.header)
         view.fullCompleteDescription.setText(arguments.argsForFullCompleted.description)
-        view.fullFinishDate.setText(arguments.argsForFullCompleted.completedTime)
+        view.fullFinishDate.setText(sdf.format(arguments.argsForFullCompleted.completedTime.toLong()))
 
         return view
     }
--- app/src/main/java/com/example/myreminders/MVVM/View/Overdue/ShowFullOverdueReminder.kt
+++ app/src/main/java/com/example/myreminders/MVVM/View/Overdue/ShowFullOverdueReminder.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.myreminders.R
 import kotlinx.android.synthetic.main.fragment_show_full_completed_reminder.view.*
+import java.text.SimpleDateFormat
 
 class ShowFullOverdueReminder : Fragment() {
     private val args by navArgs<ShowFullOverdueReminderArgs>()
@@ -18,10 +19,10 @@ class ShowFullOverdueReminder : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_show_full_completed_reminder, container, false)
-
+        val sdf = SimpleDateFormat("dd.M.yyyy")
         view.fullCompleteHeader.setText(args.argsForOverdueReminder.header)
         view.fullCompleteDescription.setText(args.argsForOverdueReminder.description)
-        view.fullFinishDate.setText(args.argsForOverdueReminder.overdueTime)
+        view.fullFinishDate.setText(sdf.format(args.argsForOverdueReminder.overdueTime.toLong()))
 
         return view
     }
